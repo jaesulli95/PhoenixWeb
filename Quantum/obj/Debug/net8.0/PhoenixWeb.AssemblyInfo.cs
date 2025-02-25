ff4cb4748b11f5a8f5e3c7d3dfb34ee5bfc2e4ff
Joshua Sullivan <jsulli0527@gmail.com>
2025-02-24 22:49:25 -0500

    Deleted Redundant files, and adding in code prep for new Database Models
    Deleted Redundant files, and adding in code prep for new Database Models

--- Quantum/obj/Debug/net8.0/PhoenixWeb.AssemblyInfo.cs
+++ Quantum/obj/Debug/net8.0/PhoenixWeb.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhoenixWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+446f4a3f0bba5f9d44365a46b4529e2dcae67d2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3115cd653f6d7b387e18066faf73e2a9a6391b4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhoenixWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhoenixWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
