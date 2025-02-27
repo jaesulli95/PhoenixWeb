c8eecaa03b1fbef772193505c9d9e03e931ff330
Joshua Sullivan <jsulli0527@gmail.com>
2025-02-27 01:16:10 -0500

    Journal Entry front end now complete
    Journal Entry front end now complete. Add Names too the Journal Entry Modal. Then implement the calls on the API and Controller sides.

--- Quantum/wwwroot/Phoenix/js/phoenix-web.js
+++ Quantum/wwwroot/Phoenix/js/phoenix-web.js
@@ -15,6 +15,51 @@ $('tr.project-task-row').on('click', function (e) {
 
 });
 
-$('tr').on('click', function (e) {
 
+function SetChecked(ElementIdName, status) {
+    if (status == "Yes") {
+        document.getElementById(ElementIdName).checked = true;
+        return;
+    } else if (status == "No") {
+        document.getElementById(ElementIdName).checked = false;
+    }
+}
+
+$('tr.entry-row').on('click', function (e) {
+
+    var $JournalData = document.getElementById("JournalData");
+    if ($JournalData.style.visibility == 'hidden') {
+        $JournalData.style.visibility = 'visible';
+    }
+
+    //Daily Stats
+    var $date = $(this).find("td:nth-child(2)").text();
+    var $water = $(this).find("td:nth-child(6)").text();
+    var $protein = $(this).find("td:nth-child(5)").text();
+    var $calories = $(this).find("td:nth-child(7)").text();
+    var $weight = $(this).find("td:nth-child(8)").text();
+
+
+    document.getElementById("Water").value = $water;
+    document.getElementById("Protein").value = $protein;
+    document.getElementById("Calories").value = $calories;
+    document.getElementById("Weight").value = $weight;
+    document.getElementById("JournalEntryDate").innerHTML = $date;
+
+    //FOOD
+    var $food = $(this).find("td:nth-child(9)").text();
+    document.getElementById("Food").value = $food;
+
+    //Checked
+    var $gym = $(this).find("td:nth-child(4)").text();
+    var $cooked = $(this).find("td:nth-child(3)").text();
+    var $mochas = $(this).find("td:nth-child(10)").text();
+    var $erotica = $(this).find("td:nth-child(11)").text();
+    var $reddit = $(this).find("td:nth-child(12)").text();
+
+    SetChecked("GymExercise", $gym);
+    SetChecked("CookedFood", $cooked);
+    SetChecked("NoMochas", $mochas);
+    SetChecked("NoErotica", $erotica);
+    SetChecked("NoReddit", $reddit);
 });
