0530511188ee7a7fb60f34b94af690eeface7b0e
Joshua Sullivan <jsulli0527@gmail.com>
2025-03-10 01:09:42 -0400

    Update Items Controller and classes

--- Quantum/Controllers/ItemsController.cs
+++ Quantum/Controllers/ItemsController.cs
@@ -1,12 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Quantum.Models;
 
 namespace PhoenixWeb.Controllers
 {
     public class ItemsController : Controller
     {
+        private IConfiguration configuration;
+        private string? ApiBaseUrl;
+
+        public ItemsController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            ApiBaseUrl = this.configuration["PhoenixLifeApiBase"];
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult?> CreateItem(Item item)
+        {
+
+            try
+            {
+                /*var Client = new HttpClient();
+                //HERE CREATE THE CLIENT TO SEND TO AN API ENDPOINT
+                HttpResponseMessage response = await Client.PostAsJsonAsync($"{this.ApiBaseUrl}/Projects/Create", project);
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    return RedirectToAction(nameof(this.Index));
+                }*/
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return RedirectToAction(nameof(this.Index));
         }
     }
 }
--- Quantum/Models/Item.cs
+++ Quantum/Models/Item.cs
@@ -1,7 +1,13 @@
-﻿namespace PhoenixWeb.Models
+﻿namespace Quantum.Models
 {
     public class Item
     {
-
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public float Price { get; set; }
+        public string Store {  get; set; }
+        public string Category { get; set; }
+        public string Notes {  get; set; }
     }
 }
