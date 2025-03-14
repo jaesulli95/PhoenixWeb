fd69b1b63ec433edb96170584ed0a26f4bdb4357
Joshua Sullivan <jsulli0527@gmail.com>
2025-03-14 00:39:19 -0400

    Added Modals, css, js files for projects.
    Added Modals, css, js files for projects.

---
+++ Quantum/wwwroot/Phoenix/js/phoenix-projects.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $('#dtProjects').DataTable({
+        dom: "<'row mb-3'<'col-sm-4'l><'col-sm-8 text-end'<'d-lg-flex justify-content-end'<'mb-3 mb-lg-0'f>B>>>t<'d-lg-flex align-items-center mt-3'<'me-auto mb-lg-0 mb-3'i><'mb-0'p>>",
+        order: [[1, 'asc']],
+        lengthMenu: [25, 50, 100],
+        responsive: true,
+        buttons: [
+            { extend: 'csv', className: 'btn btn-sm' },
+        ]
+    });
+});
--- Quantum/wwwroot/Phoenix/js/phoenix-web.js
+++ Quantum/wwwroot/Phoenix/js/phoenix-web.js
@@ -1,13 +1,4 @@
-﻿$(document).ready(function () {
-    $("#ProjectSearch").on("keyup", function () {
-        var value = $(this).val().toLowerCase();
-        $("#T_Project tr.projectrow").filter(function () {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-        });
-    });
-});
-
-$('tr.project-task-row').on('click', function (e) {
+﻿$('tr.project-task-row').on('click', function (e) {
     var $name = $(this).find("td:nth-child(1)").text();
     var $description = $(this).find("td:nth-child(2)").text();
     var $type = $(this).find("td:nth-child(3)").text();
