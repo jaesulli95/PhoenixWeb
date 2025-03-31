8075105ed88ec0a5fe0a58ba912822cfab4b0bd2
Joshua Sullivan <jsulli0527@gmail.com>
2025-03-30 22:21:12 -0400

    Expenses
    Added Expenses Tab. Modal for adding data to the database, and totals.

--- Quantum/Controllers/ExpensesController.cs
+++ Quantum/Controllers/ExpensesController.cs
@@ -1,12 +1,60 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Quantum.Models;
+using System.Diagnostics;
 
 namespace PhoenixWeb.Controllers
 {
     public class ExpensesController : Controller
     {
-        public IActionResult Index()
+        string[] ExpenseCategories = { "Rent", "Food", "Phone", "Car Insurance", "Taekwondo","Utilities", "Misc" };
+        private IConfiguration configuration;
+        private string? ApiBaseUrl;
+
+        public ExpensesController(IConfiguration configuration) {
+            this.configuration = configuration;
+            ApiBaseUrl = this.configuration["PhoenixLifeApiBase"];
+        }
+
+        public async Task<IActionResult> Index()
         {
+
+            List<Expense>? Expenses = new List<Expense>();
+            var Client = new HttpClient();
+            try
+            {
+                var response = await Client.GetAsync($"{this.ApiBaseUrl}/Expenses");
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                Expenses = JsonConvert.DeserializeObject<List<Expense>>(apiResponse);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception occured");
+            }
+
+            ViewBag.Expenses = Expenses;
+            ViewBag.ExpenseCategories = ExpenseCategories;
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddExpense(Expense expense)
+        {
+            try
+            {
+                var Client = new HttpClient();
+                HttpResponseMessage response = await Client.PostAsJsonAsync($"{this.ApiBaseUrl}/Expenses/Add", expense);
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    return RedirectToAction(nameof(this.Index));
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return RedirectToAction(nameof(this.Index));
         }
     }
 }
---
+++ Quantum/Models/Expense.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Quantum.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public float Cost { get; set; } = 0.00F;
+        public string Notes { get; set; } = "N/A";
+        public string Category { get; set; } = "Misc";
+        public string Date { get; set; } = "MM-DD-YYYY";
+    }
+}
