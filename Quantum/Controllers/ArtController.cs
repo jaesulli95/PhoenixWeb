a940c8c4223a0bbd56298cff12396464e39d1ef2
Joshua Sullivan <jsulli0527@gmail.com>
2025-05-06 08:46:00 -0400

    Update

---
+++ Quantum/Controllers/ArtController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhoenixWeb.Controllers
+{
+    public class ArtController : Controller
+    {
+        public IActionResult Gallery()
+        {
+            return View("~/Views/Art/Gallery.cshtml");
+        }
+
+
+        public IActionResult ThreeD()
+        {
+            return View("~/Views/Art/ThreeD.cshtml");
+        }
+    }
+}
